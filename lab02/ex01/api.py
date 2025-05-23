32dcdc9bb495eea1ab5434c70dcba5c5789c4292
vanquy0911 <quytan27@gmail.com>
2025-05-23 14:25:06 +0700

    add vigenere

--- lab02/ex01/api.py
+++ lab02/ex01/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
-
+from cipher.vigenere import VigenereCipher
 app = Flask(__name__)
 
 # CAESAR CIPHER ALGORITHM
@@ -25,3 +25,32 @@ def caesar_decrypt():
 # main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+# VIGENERE CIPHER ALGORITHM
+vigenere_cipher = VigenereCipher()
+
+@app.route('/api/caesar/encrypt', methods=['POST'])
+def caesar_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+# VIGENERE ENCRYPTION
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+# VIGENERE DECRYPTION
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
